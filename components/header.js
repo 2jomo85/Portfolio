e8feecf189ead7b1dc2dba2d0650a71b1189f562
jomo85 <praytop@gmail.com>
2023-05-02 16:59:46 +0900

    dotenv 패키지 추가

--- components/header.js
+++ components/header.js
@@ -4,11 +4,11 @@ import DarkModeToggleButton from "./dark-mode-toggle-button";
 export default function Header() {
   return (
     <>
-      <header className="text-gray-600 body-font">
+      <header className="body-font">
         <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
           <Link
             href="/"
-            className="flex title-font font-medium items-center mb-4 md:mb-0"
+            className="flex title-font font-medium items-center md:justify-start justify-center"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
