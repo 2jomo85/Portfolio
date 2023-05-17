48242a6aaf34f2a3b60ce9409bdf99d50f11f8c5
jomo85 <praytop@gmail.com>
2023-05-17 11:54:36 +0900

    modify hero message

--- components/home/hero.js
+++ components/home/hero.js
@@ -2,6 +2,14 @@ import Link from "next/link";
 import Animation from "@/components/home/animation";
 
 export default function Hero() {
+  const workStart = new Date("2011", "3", "4");
+  const today = new Date();
+
+  const workYearPeriod = Math.ceil(
+    Math.abs(workStart - today) / (1000 * 60 * 60 * 24 * 365)
+  );
+  console.log(workStart, today, workYearPeriod);
+
   return (
     <>
       <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
@@ -10,17 +18,18 @@ export default function Hero() {
       <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
         <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
           안녕하세요. jomo 입니다.
-          <br className="hidden lg:inline-block" />
-          오늘도 즐거운 하루!
         </h1>
+        <h2 className="title-font sm:text-2xl text-1xl mb-4 font-medium text-gray-900">
+          {workYearPeriod}년 차, 오늘도 빡!코딩 중인 개발자 입니다.
+        </h2>
         <p className="mb-8 leading-relaxed">
-          사랑의 불러 인간이 보는 소담스러운 그들에게 때까지 보라. 피에 가치를
-          인간에 구하지 끓는다. 인간은 그들은 불러 찾아다녀도, 때문이다.
-          유소년에게서 무한한 끝까지 피고, 몸이 운다. 생명을 인생에 설산에서
-          같이 두손을 것은 이것이다. 무한한 그들의 타오르고 같이 보라. 커다란
-          대고, 품었기 없으면, 심장의 가는 속에서 소리다.이것은 쓸쓸하랴? 듣기만
-          하여도 내는 실현에 수 봄바람이다. 때까지 이상의 유소년에게서 위하여서.
-          싹이 구하기 꽃 몸이 살 석가는 얼마나 있을 소리다.이것은 것이다.
+          {workYearPeriod}년 차 개발자로 MES, ERP 프로젝트를
+          개발/운영하였습니다. 주로 생산, 품질 개발을 담당했으며 필요에 따라
+          설비, 금형, 물류 등 다른 모듈의 개발도 겸하였습니다. 처음이라
+          서툴었지만 회사의 framework의 버전업 프로젝트에 참여했던 경험이
+          있습니다.
+          <br />
+          새로운 언어에 흥미를 가지고 배우는 것을 좋아합니다.
         </p>
         <div className="flex justify-center">
           <Link href="/projects">
