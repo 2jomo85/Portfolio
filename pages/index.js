7c34263e7081bac8bb373a0c04de69a22d3e32f7
jomo85 <praytop@gmail.com>
2023-05-17 11:57:16 +0900

    test index.js

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,9 @@ import { Inter } from "next/font/google";
 import Layout from "@/components/layout";
 import Hero from "@/components/home/hero";
 
+import Header from "@/components/header";
+import Footer from "@/components/footer";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
@@ -15,7 +18,12 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <section className="flex min-h-screen flex-col items-center justify-center text-gray-600 body-font">
+      {/* <section className="flex min-h-screen flex-col items-center justify-center text-gray-600">
+        <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+          <Hero />
+        </div>
+      </section> */}
+      <section className="flex flex-col items-center justify-center text-gray-600">
         <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
           <Hero />
         </div>
